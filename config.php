2644ffb048dd43b79d2347e7a31cd66ce70eb698
Akirä <151883246+Akira0202l@users.noreply.github.com>
2025-04-27 21:20:17 -0300

    fdgfdbvgfd

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php
+$dbHost = 'Localhost';
+$dbUsername = 'root';
+$dbPassword = '';
+$dbName = 'Cadastro';
+
+$conexao = new mysqli ($dbHost, $dbUsername, $dbPassword, $dbName);
+
+if ($conexao->connect_error)
+{
+    echo "Erro";
+
+}
+
+else 
+{ 
+
+}
+
+
+
+?>
